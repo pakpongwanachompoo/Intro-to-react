c1791fea56993f6120b621341ca9a0960fa3178d
vorawit-s63 <s6301012630168@email.kmutnb.ac.th>
2022-01-23 01:04:44 +0700

    add jump to for history button

--- src/game.js
+++ src/game.js
@@ -10,12 +10,13 @@ class Game extends React.Component {
         history: [{
             squares: Array(9).fill(null)
             }],
-            xIsNext: true
+            stepNumber: 0,
+            xIsNext: true,
         };
     }
     
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (CalculateWinner(squares) || squares[i]) {
@@ -26,12 +27,22 @@ class Game extends React.Component {
             history: history.concat([{
             squares: squares
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+          xIsNext: (step % 2) === 0,
+        });
+      }
+
+      
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = CalculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -39,7 +50,7 @@ class Game extends React.Component {
               'Go to move #' + move :
               'Go to game start';
             return (
-              <li>
+              <li key={move}>
                 <button onClick={() => this.jumpTo(move)}>{desc}</button>
               </li>
             );
