85d6f76e37484d4f9e799c53626befa20d2b33a9
MoDZi ! <zerostriketeam@gmail.com>
2022-01-22 15:29:01 +0700

    Add constructor,change value in tag square and add function onclick

--- src/board.js
+++ src/board.js
@@ -5,8 +5,18 @@ import  Square from './square'
 
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square 
+                value={this.state.squares[i]} 
+                onClick={() => this.handleClick(i)}
+               />;
     }
 
     render() {
