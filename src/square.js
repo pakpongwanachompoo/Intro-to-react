e289eea52667c9b00cb66154ced9828e30312403
vorawit-s63 <s6301012630168@email.kmutnb.ac.th>
2022-01-22 23:03:35 +0700

    change class square to function

--- src/square.js
+++ src/square.js
@@ -2,16 +2,11 @@ import React from 'react';
 import './index.css';
 
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props){
+    return(
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
-
 export default Square;
